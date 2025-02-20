63f103809108d5840c52519cf820aa815279d63e
Lorenz Velasco <backupbituinvelascogabriel21@gmail.com>
2025-02-20 11:01:01 +0000

    idk

---
+++ test.java
@@ -0,0 +1,25 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Codespaces!");
+
+        int num = 21; 
+
+        System.out.println(num);
+
+        if (num == 21) {
+           while (true) {
+            for (int i = 0; i < 20; i++) {
+                System.out.println("hello");
+                break;
+                
+            }
+           }
+        } else {
+            System.out.println("hello but no repited shit");
+        }
+
+
+
+        
+    }
+}
